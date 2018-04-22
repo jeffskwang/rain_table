fd4e4691e39a5a08536ca640f0278c730f75437c
jeffskwang <30662804+jeffskwang@users.noreply.github.com>
2018-04-22 15:30:27 -0500

    fix
    fix

--- run_rain_table.py
+++ run_rain_table.py
@@ -4,9 +4,11 @@ import os, sys
 this_dir = os.path.dirname(__file__)
 
 # if commands given
-if len(sys.argv) > 0:
+if len(sys.argv) > 1:
     # argument for python 3 runtime
     if sys.argv[1] == '--python3':
         os.system('python3 ' + os.path.join(this_dir, 'src', 'rain_table.py'))
     else:
         os.system('python ' + os.path.join(this_dir, 'src', 'rain_table.py'))
+else: 
+    os.system('python ' + os.path.join(this_dir, 'src', 'rain_table.py'))
