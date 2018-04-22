22d86a75adfb1bdcf9a2350b9228affa1a2be2b3
amoodie <amoodie@rice.edu>
2018-04-22 13:53:19 -0500

    update readme

--- run_rain_table.py
+++ run_rain_table.py
@@ -1,5 +1,12 @@
-import os
+import os, sys
 
-
+# the file location
 this_dir = os.path.dirname(__file__)
-os.system('python ' + os.path.join(this_dir, 'src', 'rain_table.py'))
+
+# if commands given
+if len(sys.argv) > 0:
+    # argument for python 3 runtime
+    if sys.argv[1] == '--python3':
+        os.system('python3 ' + os.path.join(this_dir, 'src', 'rain_table.py'))
+    else:
+        os.system('python ' + os.path.join(this_dir, 'src', 'rain_table.py'))
